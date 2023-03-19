569136f862816302fc36ef7d3e2f2594b01f1080
Aleix <nitjac@gmail.com>
2023-03-19 10:27:36 +0100

    Hamburguer menú modified / Toggle Darktheme / Light-theme

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,24 @@
 // HAMBURGUER MENÚ
+const themeSwitch = document.getElementById("theme-switch");
+const themeBody = document.getElementById("theme-body");
+
+themeSwitch.addEventListener("change", () => {
+  if (themeSwitch.checked) {
+    themeBody.classList.add("dark-mode");
+  } else {
+    themeBody.classList.remove("dark-mode");
+  }
+});
+
+
+// const toggleThemeBtn = document.getElementById('toggle-theme-btn');
+// const bodyEl = document.body;
+
+// toggleThemeBtn.addEventListener('click', () => {
+//   bodyEl.classList.toggle('dark-mode');
+// });
+
+
 const hamburger = document.querySelector(".hamburgerMenu")
 const navMenu = document.querySelector(".menuList")
 
